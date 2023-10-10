d44a774e1ac44a4e2361609120a15e7afeefa206
Tomoki108 <shinonome81@gmail.com>
2023-10-10 14:30:30 +0900

    using gorm, get data from mysql

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+	"you-tree-backend/database"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Tree struct {
+	Id          int
+	Title       string
+	Description string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
 
 func main() {
+	var tree Tree
+	database.Db.First(&tree, 1)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+		c.JSON(200, tree)
 	})
 	r.Run()
 }
