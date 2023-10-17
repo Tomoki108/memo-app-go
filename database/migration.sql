3841a06ae2534ebd1b49cc3a39590dda2be3c6dc
Tomoki108 <shinonome81@gmail.com>
2023-10-17 17:09:42 +0900

    more replace

--- database/migration.sql
+++ database/migration.sql
@@ -1,15 +1,13 @@
-CREATE TABLE trees (
+CREATE TABLE memos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
+    body VARCHAR(255),
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL
 );
 
-
-INSERT INTO trees (title, description, created_at, updated_at)
+INSERT INTO memos (title, body, created_at, updated_at)
 VALUES
-    ('Python', 'Pythonプログラミング言語の基本をマスターします。', '2023-10-07 08:00:00', '2023-10-07 08:00:00'),
-    ('JavaScript', 'JavaScriptのフロントエンド開発を学びます。', '2023-10-07 11:15:00', '2023-10-07 11:15:00'),
-    ('AWSクラウド', 'Amazon Web Servicesの基本を理解します。', '2023-10-07 12:45:00', '2023-10-07 12:45:00')
+    ('Python', 'データサイエンスや競技プログラミングの分野で使われます', '2023-10-07 08:00:00', '2023-10-07 08:00:00'),
+    ('JavaScript', '主にフロントエンド開発に用いられます', '2023-10-07 11:15:00', '2023-10-07 11:15:00')
 ;
--- main.go
+++ main.go
@@ -8,69 +8,69 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Tree struct {
-	Id          int       `json:"id" gorm:"primary_key"`
-	Title       string    `json:"title" gorm:"not null"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+type Memo struct {
+	Id        int       `json:"id" gorm:"primary_key"`
+	Title     string    `json:"title" gorm:"not null"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/trees", func(c *gin.Context) {
-		var trees []Tree
-		database.Db.Find(&trees)
-		c.JSON(200, trees)
+	r.GET("/memos", func(c *gin.Context) {
+		var memos []Memo
+		database.Db.Find(&memos)
+		c.JSON(200, memos)
 	})
 
-	r.POST("/trees", func(c *gin.Context) {
-		var tree Tree
-		if err := c.ShouldBindJSON(&tree); err != nil {
+	r.POST("/memos", func(c *gin.Context) {
+		var memo Memo
+		if err := c.ShouldBindJSON(&memo); err != nil {
 			fmt.Println(err)
 			c.AbortWithStatus(400)
-		} else if tree.Title == "" {
+		} else if memo.Title == "" {
 			fmt.Print("title is empty yo")
 			c.AbortWithStatus(400)
 		} else {
-			database.Db.Create(&tree)
-			c.JSON(200, tree)
+			database.Db.Create(&memo)
+			c.JSON(200, memo)
 		}
 	})
 
-	r.GET("/trees/:id", func(c *gin.Context) {
-		var tree Tree
-		if err := database.Db.Where("id = ?", c.Param("id")).First(&tree).Error; err != nil {
+	r.GET("/memos/:id", func(c *gin.Context) {
+		var memo Memo
+		if err := database.Db.Where("id = ?", c.Param("id")).First(&memo).Error; err != nil {
 			fmt.Println(err)
 			c.AbortWithStatus(404)
 		} else {
-			c.JSON(200, tree)
+			c.JSON(200, memo)
 		}
 	})
 
-	r.PUT("/trees/:id", func(c *gin.Context) {
-		var tree Tree
-		if err := database.Db.Where("id = ?", c.Param("id")).First(&tree).Error; err != nil {
+	r.PUT("/memos/:id", func(c *gin.Context) {
+		var memo Memo
+		if err := database.Db.Where("id = ?", c.Param("id")).First(&memo).Error; err != nil {
 			fmt.Println(err)
 			c.AbortWithStatus(404)
 		} else {
-			if err := c.ShouldBindJSON(&tree); err != nil {
+			if err := c.ShouldBindJSON(&memo); err != nil {
 				c.AbortWithStatus(400)
 			} else {
-				database.Db.Save(&tree)
-				c.JSON(200, tree)
+				database.Db.Save(&memo)
+				c.JSON(200, memo)
 			}
 		}
 	})
 
-	r.DELETE("/trees/:id", func(c *gin.Context) {
-		var tree Tree
-		if err := database.Db.Where("id = ?", c.Param("id")).First(&tree).Error; err != nil {
+	r.DELETE("/memos/:id", func(c *gin.Context) {
+		var memo Memo
+		if err := database.Db.Where("id = ?", c.Param("id")).First(&memo).Error; err != nil {
 			fmt.Println(err)
 			c.AbortWithStatus(404)
 		} else {
-			database.Db.Delete(&tree)
+			database.Db.Delete(&memo)
 			c.JSON(200, gin.H{"id #" + c.Param("id"): "deleted"})
 		}
 	})
