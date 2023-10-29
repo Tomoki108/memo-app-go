67e13ddc7fcd333f49a8c5e201a96a7905e12d11
Tomoki108 <shinonome81@gmail.com>
2023-10-29 17:00:27 +0900

    remove unused code

--- main.go
+++ main.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
 
 	"memo-app-go/router"
 )
-
-type Memo struct {
-	Id        int       `json:"id" gorm:"primary_key"`
-	Title     string    `json:"title" gorm:"not null"`
-	Body      string    `json:"body"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-}
 
 func main() {
 	engine := gin.Default()
